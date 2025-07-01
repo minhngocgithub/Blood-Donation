de37fdadf55c4bae60d1f01eb6c8ff70b9fc02e7
Minh Ngoc Github <nguyenminhngoc11102004@gmail.com>
2025-07-01 14:49:10 +0700

    [#3.2 Update ApplicationDbContext]

---
+++ Services/Interfaces/IAccountService.cs
@@ -0,0 +1,10 @@
+﻿using Blood_Donation_Website.Models.Entities;
+using Blood_Donation_Website.Models.ViewModels.Account;
+
+namespace Blood_Donation_Website.Services.Interfaces
+{
+    public class IAccountService
+    {
+        
+    }
+}
