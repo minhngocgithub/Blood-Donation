e0ff378aac4141893911aa595eade193c1d2c55c
Minh Ngoc Github <nguyenminhngoc11102004@gmail.com>
2025-07-03 07:37:38 +0700

    [#4 Create relationship for database]

---
+++ Data/Configurations/NewsCategoryConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Blood_Donation_Website.Models.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blood_Donation_Website.Data.Configurations
+{
+    public class NewsCategoryConfiguration : IEntityTypeConfiguration<NewsCategory>
+    {
+        public void Configure(EntityTypeBuilder<NewsCategory> builder)
+        {
+            builder.ToTable("NewsCategories");
+
+            builder.HasKey(nc => nc.CategoryId);
+
+            builder.Property(nc => nc.CategoryId)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(nc => nc.CategoryName)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(nc => nc.Description)
+                .HasMaxLength(200);
+
+            builder.Property(nc => nc.IsActive)
+                .HasDefaultValue(true);
+        }
+    }
+}
