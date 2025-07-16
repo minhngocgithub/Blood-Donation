08f236432112d6f4e59bdb40a0df5f1752b48b33
dxtruong16a4 <dxtruong.dhti16a4hn@sv.uneti.edu.vn>
2025-07-16 07:47:12 +0700

    Refactor something

--- Controllers/Home/HomeController.cs
+++ Controllers/Home/HomeController.cs
@@ -6,11 +6,8 @@ namespace Blood_Donation_Website.Controllers.Home
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
         }
 
         public IActionResult Index()
@@ -54,4 +51,4 @@ namespace Blood_Donation_Website.Controllers.Home
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
-} 
+}
